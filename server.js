4ec0fbb38d58a55d502aa531128603a631e9621e
Ali Salehi <parkour.runner@gmail.com>
2024-02-22 17:07:38 +0300

    enable cors

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const productController = require('./controllers/wordController');
+const wordController = require('./controllers/wordController');
 const { errorHandler } = require('./middlewares/errorMiddleware');
 
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', productController);
+app.use('/api', wordController);
 
 
 app.use(errorHandler);
